36afe23b690606c6dfaf32cefe5b668b10be8fda
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-06 14:08:52 +0300

    Add files via upload

---
+++ 04-Python-OOP/06-Polymorphism-and-Abstraction/solutions.py
@@ -0,0 +1,145 @@
+# # # Exercise
+
+# # 01. Vehicle
+# from abc import ABC, abstractmethod
+#
+#
+# class Vehicle(ABC):
+#
+#     def __init__(self, fuel_quantity, fuel_consumption):
+#         self.fuel_quantity = fuel_quantity
+#         self.fuel_consumption = fuel_consumption
+#
+#     @abstractmethod
+#     def drive(self, distance):
+#         pass
+#
+#     @abstractmethod
+#     def refuel(self, fuel):
+#         pass
+#
+#
+# class Car(Vehicle, ABC):
+#     def __init__(self, fuel_quantity, fuel_consumption):
+#         super().__init__(fuel_quantity, fuel_consumption)
+#
+#     def drive(self, distance):
+#         needed_fuel = distance * (self.fuel_consumption + 0.9)
+#         if needed_fuel <= self.fuel_quantity:
+#             self.fuel_quantity -= needed_fuel
+#             return self.fuel_quantity
+#
+#     def refuel(self, fuel):
+#         self.fuel_quantity += fuel
+#
+#
+# class Truck(Vehicle, ABC):
+#
+#     def __init__(self, fuel_quantity, fuel_consumption):
+#         super().__init__(fuel_quantity, fuel_consumption)
+#
+#     def drive(self, distance):
+#         needed_fuel = distance * (self.fuel_consumption + 1.6)
+#         if needed_fuel <= self.fuel_quantity:
+#             self.fuel_quantity -= needed_fuel
+#             return self.fuel_quantity
+#
+#     def refuel(self, fuel):
+#         self.fuel_quantity += 0.95 * fuel
+
+
+# # 02. Groups
+#
+# class Person:
+#
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#     def full_name(self):
+#         return f'{self.name} {self.surname}'
+#
+#     def __str__(self):
+#         return f'{self.name} {self.surname}'
+#
+#     def __add__(self, other):
+#         return f'{self.name} {other.surname}'
+#
+#
+# class Group:
+#
+#     def __init__(self, name: str, people: list):
+#         self.name = name
+#         self.people = people
+#
+#     def __len__(self):
+#         return len(self.people)
+#
+#     def __add__(self, other):
+#         new_name = f'{self.name} {other.name}'
+#         members = self.people + other.people
+#         return Group(new_name, members)
+#
+#     def __str__(self):
+#         return f"Group {self.name} with members {', '.join([str(p) for p in self.people])}"
+#
+#     def __getitem__(self, index):
+#         return f'Person {index}: {self.people[index]}'
+
+
+# # 03. Accounts
+# class Account:
+#     def __init__(self, owner, amount=0):
+#         self.owner = owner
+#         self.amount = amount
+#         self._transactions = []
+#
+#     @property
+#     def balance(self):
+#         return sum(self._transactions) + self.amount
+#
+#     def handle_transaction(self, transaction_amount):
+#         if self.balance + transaction_amount < 0:
+#             raise ValueError("sorry cannot go in debt!")
+#
+#         self._transactions.append(transaction_amount)
+#
+#         return f"New balance: {self.balance}"
+#
+#     def add_transaction(self, amount):
+#         if not isinstance(amount, int):
+#             raise ValueError("please use int for amount")
+#
+#         return self.handle_transaction(amount)
+#
+#     def __str__(self):
+#         return f"Account of {self.owner} with starting amount: {self.amount}"
+#
+#     def __repr__(self):
+#         return f"Account({self.owner}, {self.amount})"
+#
+#     def __len__(self):
+#         return len(self._transactions)
+#
+#     def __getitem__(self, idx):
+#         return self._transactions[idx]
+#
+#     def __reversed__(self):
+#         return reversed(self._transactions)
+#
+#     def __gt__(self, other):
+#         return self.balance > other.balance
+#
+#     def __ge__(self, other):
+#         return self.balance >= other.balance
+#
+#     def __eq__(self, other):
+#         return self.balance == other.balance
+#
+#     def __add__(self, other):
+#         new_account = Account(f"{self.owner}&{other.owner}", self.amount + other.amount)
+#         new_account._transactions = self._transactions + other._transactions
+#         return new_account
+
+
+# 04. Wild Farm
