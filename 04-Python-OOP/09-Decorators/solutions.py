f147a0874cc7a47ef59e90bb18b06e4cc12a1b98
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-11 17:44:43 +0300

    Add files via upload

--- 04-Python-OOP/09-Decorators/solutions.py
+++ 04-Python-OOP/09-Decorators/solutions.py
@@ -219,3 +219,28 @@
 #
 # add(2, 2)
 # mult(6, 4)
+
+
+# # 8. Execution Time
+# from time import time
+# 
+#
+# def exec_time(func):
+#     def wrapper(*args):
+#         start = time()
+#         func(*args)
+#         end = time()
+#         return end - start
+#
+#     return wrapper
+#
+#
+# @exec_time
+# def loop(start, end):
+#     total = 0
+#     for x in range(start, end):
+#         total += x
+#     return total
+#
+#
+# print(loop(1, 10000000))
