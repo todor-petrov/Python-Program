50306ab6ee06331d29e5f5808107f625c5688171
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-03 12:47:08 +0300

    Add files via upload

--- 03-Advanced/06-File-Handling/examples.py
+++ 03-Advanced/06-File-Handling/examples.py
@@ -1,13 +1,21 @@
-# 01. File Opener
+# # 01. File Opener
 import os
 
 # try:
 #     file = open('text.txt')
-#     print('File found.')
+#     print('File found')
 # except FileNotFoundError:
-#     print('File not found!')
+#     print('File not found')
 
-if os.path.exists('text.txt'):
-    print('File found.')
-else:
-    print('File not found!')
+# if os.path.exists('text.txt'):
+#     print('File found')
+# else:
+#     print('File not found')
+
+
+# # 02. File Reader
+# try:
+#     with open('numbers.txt', 'r') as file:
+#         print(sum(int(line) for line in file.readlines()))
+# except FileNotFoundError:
+#     print('File not found')
