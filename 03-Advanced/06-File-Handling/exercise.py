b2ae4c457e8a5a1af43d744e974c85ea1d2a5bd5
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-04 10:41:36 +0300

    Add files via upload

---
+++ 03-Advanced/06-File-Handling/exercise.py
@@ -0,0 +1,9 @@
+# # 01. Even Lines
+# symbols = ['-', ',', '.', '!', '?']
+# with open('text.txt', 'r') as even_lines_file:
+#     text = even_lines_file.readlines()
+# for row in range(0, len(text), 2):
+#     for symbol in symbols:
+#         text[row] = text[row].replace(symbol, '@')
+#
+#     print(*text[row].split()[::-1])
