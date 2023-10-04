865b335cd9229fb6f157048ff70a7b18d3563691
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-04 11:23:37 +0300

    Add files via upload

--- 03-Advanced/06-File-Handling/exercise.py
+++ 03-Advanced/06-File-Handling/exercise.py
@@ -7,3 +7,26 @@
 #         text[row] = text[row].replace(symbol, '@')
 #
 #     print(*text[row].split()[::-1])
+
+
+# # Line Numbers
+# from string import punctuation
+#
+# output_file = open('output.txt', 'a')
+# with open('text.txt', 'r') as text_file:
+#     text = text_file.readlines()
+#
+# for i in range(len(text)):
+#     row = text[i]
+#
+#     letters, marks = 0, 0
+#
+#     for symbol in row:
+#         if symbol.isalpha():
+#             letters += 1
+#         elif symbol in punctuation:
+#             marks += 1
+#
+#     output_file.write(f"Line: {i+1}: {''.join(row[:-1])} ({letters})({marks})\n")
+#
+# output_file.close()
