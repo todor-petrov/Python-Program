83df9f544226b47f4b998bca54cddaf516424565
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-05 12:15:53 +0300

    Add files via upload

--- 03-Advanced/06-File-Handling/exercise.py
+++ 03-Advanced/06-File-Handling/exercise.py
@@ -30,3 +30,41 @@
 #     output_file.write(f"Line: {i+1}: {''.join(row[:-1])} ({letters})({marks})\n")
 #
 # output_file.close()
+
+
+# # 03. File Manipulator
+# import os
+#
+# while True:
+#     info = input().split('-')
+#
+#     if info[0] == 'Create':
+#         file = open(f'{info[1]}', 'w')
+#         file.close()
+#
+#     elif info[0] == 'Add':
+#         with open(f'{info[1]}', 'a') as file:
+#             file.write(f'{info[2]}\n')
+#
+#     elif info[0] == 'Replace':
+#         try:
+#             with open(f'{info[1]}', 'r') as file:
+#                 text = file.readlines()
+#
+#             file = open(f'{info[1]}', 'w')
+#             for i in range(len(text)):
+#                 text[i] = text[i].replace(info[2], info[3])
+#             file.write(''.join(text))
+#             file.close()
+#
+#         except FileNotFoundError:
+#             print('An error occurred')
+#
+#     elif info[0] == 'Delete':
+#         try:
+#             os.remove(f'{info[1]}')
+#         except FileNotFoundError:
+#             print('An error occurred')
+#
+#     else:
+#         break
