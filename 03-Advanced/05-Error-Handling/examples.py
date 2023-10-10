dda994e5f7a14e9ae233d368cda9cfdaebfbf3c7
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-10 08:49:09 +0300

    Add files via upload

--- 03-Advanced/05-Error-Handling/examples.py
+++ 03-Advanced/05-Error-Handling/examples.py
@@ -26,10 +26,10 @@
 
 
 # # 03. Repeat Text
-try:
-    text = input()
-    times = int(input())
-    print(text * times)
-
-except ValueError:
-    print("Variable times must be an integer")
+# try:
+#     text = input()
+#     times = int(input())
+#     print(text * times)
+#
+# except ValueError:
+#     print("Variable times must be an integer")
